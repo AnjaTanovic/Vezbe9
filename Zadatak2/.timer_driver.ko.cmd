32a577d77b7b6dff20c84584dd0a92d21d84a1d2
AnjaTanovic <anjatanovic99@gmail.com>
2021-12-21 22:01:26 +0100

    obrisani fajlovi

--- Zadatak2/.timer_driver.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/anjat/GIT/Vezbe9/Zadatak2/timer_driver.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/anjat/GIT/Vezbe9/Zadatak2/timer_driver.ko /home/anjat/GIT/Vezbe9/Zadatak2/timer_driver.o /home/anjat/GIT/Vezbe9/Zadatak2/timer_driver.mod.o ;  true
--- Zadatak2/.tmp_versions/timer_driver.mod
+++
@@ -1,3 +0,0 @@
-/home/anjat/GIT/Vezbe9/Zadatak2/timer_driver.ko
-/home/anjat/GIT/Vezbe9/Zadatak2/timer_driver.o
-
